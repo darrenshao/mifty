aaf58b588b2cdc5e48d7d3475579bbd04e2bb7a8
darrenshao <shcniat2012@163.com>
2016-06-12 10:42:04 +0800

    delete all files and replace with new files

--- src/main/java/com/shc/mifty/example/example.thrift
+++
@@ -1,6 +0,0 @@
-namespace java com.shc.mifty.example
-
-service ExampleService {
-   	string sayHello(1:string params, 2:bool isEncrypt),
-	string echo(1:string params, 2:bool isEncrypt)
-}
--- src/main/java/com/shc/mifty/service/thrift/demo.thrift
+++
@@ -1,6 +0,0 @@
-/* define your thrift service here*/
-namespace com.shc.mifty.service.gen
-
-service demoService{
-	string demoSay(1:string params, 2:bool isEncrypt);
-}
